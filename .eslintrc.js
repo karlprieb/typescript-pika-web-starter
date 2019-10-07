8d576b21b39e1362da45d5ce532131fce41658c0
Karl Prieb <karl@prieb.me>
2019-10-07 10:45:39 -0300

    build: add jest support

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended', // Recommended rules from @typescript-eslint/eslint-plugin
     'standard', // Recommended rules from StandardJS
     'prettier/@typescript-eslint', // Disable ESLint rules that would conflict with prettier
-    'plugin:prettier/recommended' // Displays prettier errors as ESLint errors
+    'plugin:prettier/recommended', // Displays prettier errors as ESLint errors
   ],
   rules: {
-    '@typescript-eslint/indent': 'off' // This is the job of StandardJS
-  }
-};
+    '@typescript-eslint/indent': 'off', // This is the job of StandardJS
+  },
+  env: {
+    jest: true,
+  },
+}
