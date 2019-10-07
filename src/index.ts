e4293ed658faed602c05025333a59e5ea6f8916c
Karl Prieb <karl@prieb.me>
2019-10-07 13:40:42 -0300

    build: add browsersync

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { add, double } from './lib/numbers'
 
-console.log(double(4))
-console.log(add(2, 2))
+const $app = document.getElementById('app')
+
+const template = `
+  <p>Double of 4: ${double(4)}</p>
+  <p>2 + 2: ${add(2, 2)}</p>
+`
+
+if ($app) {
+  $app.innerHTML = template
+}
